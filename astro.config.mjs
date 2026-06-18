526d6c0d4e9e53344527c55145690bcfe84e2e0d
Manuel-2011 <mosquera.manuel2011@gmail.com>
2026-06-18 11:59:56 -0500

    Minor fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Set `site` and `base` for GitHub Pages project sites.
-  // Update the path to match your repository name if different.
-  site: 'https://manuel-mosquera.github.io/personal-website',
-  base: '/personal-website/',
+  site: 'https://manuel-2011.github.io',
+  base: '/manuel-mosquera.github.io/',
   integrations: [tailwind({ applyBaseStyles: false })],
 });
